58e5f3af6b76b9463f503ea89c301cbc149cd49a
renatão nóbrega <paulo05renato@gmail.com>
2023-09-16 16:04:38 -0300

    test cases for update user

--- tests/service/test_service_user.py
+++ tests/service/test_service_user.py
@@ -65,3 +65,37 @@ class TestServiceUser:
 
         assert response_assert == response
 
+    def test_update_user(self):
+        service = ServiceUser()
+        name_user = "renatão"
+        response_assert = "Job de usuário atualizado"
+
+        service.add_user(name_user, "development")
+        response = service.update_user(name_user, "jardineiro")
+
+        assert response_assert == response
+
+    def test_update_user_invalid_name(self):
+        service = ServiceUser()
+        response_assert = "Nome inválido"
+
+        response = service.update_user(None, "jardineiro")
+
+        assert response_assert == response
+
+    def test_update_user_invalid_job(self):
+        service = ServiceUser()
+        response_assert = "Job inválido"
+
+        response = service.update_user("renatão", None)
+
+        assert response_assert == response
+
+    def test_update_user_doesnt_exist(self):
+        service = ServiceUser()
+        response_assert = "Não existe usuário com esse nome"
+
+        response = service.update_user("carina", "jardineira")
+
+        assert response_assert == response
+
