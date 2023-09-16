47e94ac6b02d620cba878edfb956e912c87307da
renatão nóbrega <paulo05renato@gmail.com>
2023-09-16 16:34:26 -0300

    name of team people in examples

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ from src.service.service_user import ServiceUser
 
 service = ServiceUser()
 
-print(service.add_user("renatão", "development"))
+print(service.add_user("ermeson", "qa"))
 print(service.add_user(None, "consultor"))
 print(service.add_user("renatão", None))
 print(service.add_user(0, "consultor"))
-print(service.add_user("renatão", 0))
+print(service.add_user("camila", 0))
 
-print(service.add_user("renatão", "development"))
+print(service.add_user("carina", "development"))
 
 print(service.store.bd)
 
-# print(service.remove_user("renatão"))
+# print(service.remove_user("carina"))
 #
 # print(service.store.bd)
-
 
 print(service.store.bd[0].job)
 
